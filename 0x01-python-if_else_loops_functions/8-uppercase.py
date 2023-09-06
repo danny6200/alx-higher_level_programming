d13a8145666af2896737c84f6d92f474a3f1a50f
danny6200 <ugookorodaniel620@gmail.com>
2023-09-06 15:05:50 -0700

    Reduce: line 5 in script 8 to fewer lines

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    if str == "":
+        print(" ")
     for i, c in enumerate(str):
         if c >= chr(97) and c <= chr(122):
-            print("{}".format(chr(ord(c) - ord('a') + ord('A'))), end="" if i < len(str) - 1 else "\n")
+            print("{}".format(chr(ord(c) - 32)), end="" if i < len(str) - 1 else "\n")
         else:
             print("{}".format(c), end="" if i < len(str) - 1else "\n")
