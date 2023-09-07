ebf1486f0472b827dae6278945bbf2106e5823e3
danny6200 <ugookorodaniel620@gmail.com>
2023-09-07 04:25:14 -0700

    Add: script for task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ln = len(sys.argv)
+    if ln == 1:
+        print("{} argument.".format(ln-1))
+    elif ln == 2:
+        print("{} argument:".format(ln-1))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(ln-1))
+        for i, s in enumerate(sys.argv):
+            if i == 0:
+                continue
+            else:
+                print("{}: {}".format(i, s))
