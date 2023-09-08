37ab0f94c1dc734678b2eb200896ef760005aa92
danny6200 <ugookorodaniel620@gmail.com>
2023-09-08 01:44:23 -0700

    Modify: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     ln = len(sys.argv)
     if ln == 1:
-        print("{} argument.".format(ln-1))
+        print("{} arguments.".format(ln-1))
     elif ln == 2:
         print("{} argument:".format(ln-1))
         print("{}: {}".format(1, sys.argv[1]))
