046dfec449b85305fa4bb3459ec78e02721b1415
danny6200 <ugookorodaniel620@gmail.com>
2023-09-10 06:38:15 -0700

    Modify: script for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, n in enumerate(row):
-            print("{}".format(n), end=" " if i < len(row) else "\n")
+            print("{:d}".format(n), end=" " if i < len(row) else "\n")
         print()
