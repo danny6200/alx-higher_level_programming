0ab231a8c7d3664d86bb97998eb37e5850df9c54
danny6200 <ugookorodaniel620@gmail.com>
2023-09-12 12:18:53 -0700

    Remove: space character from end of row in task 6 script

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        if len(row) == 0:
+            print()
+            return
         for i, n in enumerate(row):
-            print("{:d}".format(n), end=" " if i < len(row) else "\n")
+            print("{:d}".format(n), end=" " if i < len(row) - 1 else "\n")
         print()
