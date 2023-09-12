4814427b29104ee73926414b164dc1b2e8145281
danny6200 <ugookorodaniel620@gmail.com>
2023-09-12 12:15:03 -0700

    Modify: 'None' edge case in script for task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0 or my_list == None:
+    if my_list is None:
+        return
+    if len(my_list) == 0:
         return
     ln = len(my_list) - 1
     for i in range(ln, -1, -1):
