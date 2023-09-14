329a4116cc28003c4e5ffc08a4db2e08353fef26
danny6200 <ugookorodaniel620@gmail.com>
2023-09-14 05:51:11 -0700

    Add: script for task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary is None or key is None:
+        return a_dictionary
+    if key not in a_dictionary.keys():
+        return a_dictionary
+    del a_dictionary[key]
