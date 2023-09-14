04baac971d915706cc5ecb93c6dc5b3bcc4d6b3f
danny6200 <ugookorodaniel620@gmail.com>
2023-09-14 04:16:53 -0700

    Modify: comment in task 1 script

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,12 @@
 def search_replace(my_list, search, replace):
     if my_list is None:
         return my_list
-#new_list = my_list[:]
-#    for i in new_list:
-#        if i == search:
-#            i = replace
     return [replace if i == search else i for i in my_list]
+
+
+"""
+    new_list = my_list[:]
+    for i in new_list:
+        if i == search:
+            i = replace
+"""
