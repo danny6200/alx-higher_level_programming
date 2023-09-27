06022c65d3362640d3dfbf73565b164ebe73e54e
danny6200 <ugookorodaniel620@gmail.com>
2023-09-27 01:57:25 -0700

    Modify: 2-square.py to PEP8 standards

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 '''This is amodule-level docstring'''
+
+
 class Square:
     '''This is a docstring for the Square class.
 
-    This docstring provides an overview of what the class does and any important information
+    This docstring provides an overview of what
+    the class does and any important information
     about its usage, attributes, and methods.
 
     Attributes:
@@ -15,7 +18,7 @@ class Square:
         method1(args): Description of method1.
         method2(args): Description of method2.
     '''
-    
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
