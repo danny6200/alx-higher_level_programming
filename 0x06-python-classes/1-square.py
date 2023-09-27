057a1916fd36164a055f25496542e8f3b64f4db6
danny6200 <ugookorodaniel620@gmail.com>
2023-09-26 20:37:24 -0700

    Add: docstring to 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
+'''This is amodule-level docstring'''
 class Square:
+    '''This is a docstring for the Square class.
+
+    This docstring provides an overview of what the class does and any important information
+    about its usage, attributes, and methods.
+
+    Attributes:
+        __size (number): Description of attribute1.
+        attribute2 (type): Description of attribute2.
+
+    Methods:
+        method1(args): Description of method1.
+        method2(args): Description of method2.
+    '''
     def __init__(self, size):
         self.__size = 3
