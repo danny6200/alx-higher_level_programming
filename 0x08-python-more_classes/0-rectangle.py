3afb804e4d5bb01ccc7b348d24291caf5ba5744e
danny6200 <ugookorodaniel620@gmail.com>
2023-10-02 22:24:38 +0100

    ,
    Add: 0-rectangle.py/.txt

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This module provides the Rectangle Class
+
+    Usage:
+    ------
+        >>> Rectangle = __import__('0-rectangle').Rectangle
+        >>> my_rectangle = Rectangle()
+"""
+
+
+class Rectangle:
+    """
+        This class provides the blue print to
+        create any Rectangular object.
+
+        Attributes:
+        -----------
+
+        Methods:
+        -------
+
+    """
+    pass
