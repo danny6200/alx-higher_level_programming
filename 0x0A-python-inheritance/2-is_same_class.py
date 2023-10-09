16030a66574e900db06a4b4c01feac2a4116318c
danny6200 <ugookorodaniel620@gmail.com>
2023-10-09 23:06:56 +0100

    Add: documentation to 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    """
+        This function checks if an instance
+        or object of a class belongs to that
+        class
+    """
     if obj is None or a_class is None:
         return
     if not isinstance(obj, a_class):
