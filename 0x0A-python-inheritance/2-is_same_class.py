e08084a88a9a25d23002e2f8b491c229429b3c60
danny6200 <ugookorodaniel620@gmail.com>
2023-10-09 23:14:45 +0100

    Remove: None edge case condition

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,14 +3,14 @@
     This module supplies the is_same_class
     function.
 """
+
+
 def is_same_class(obj, a_class):
     """
         This function checks if an instance
         or object of a class belongs to that
         class
     """
-    if obj is None or a_class is None:
-        return
     if not isinstance(obj, a_class):
         return False
     return True
