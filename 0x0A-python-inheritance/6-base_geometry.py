f13c444aa1a161023874513fd987bdc30682f4e5
danny6200 <ugookorodaniel620@gmail.com>
2023-10-10 00:29:58 +0100

    Add: 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    This module supplies the base geometry
+    class
+"""
+
+
+class BaseGeometry():
+    """
+        This is the base geometry class
+
+        To be improved upon in the next task
+    """
+    pass
+
+    def area(self):
+        """
+            This function should print the area
+            of a given shape
+        """
+        raise Exception("area() is not implemented")
