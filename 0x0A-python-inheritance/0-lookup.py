fd620a5e3a66fbe231d9cc88ef8826f836579440
danny6200 <ugookorodaniel620@gmail.com>
2023-10-09 23:18:01 +0100

    Add: module level and function level doumentation to 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
+"""
+    This module supplies lookup function
+
+    It is used to loop up the methods
+    and attributes of an object
+"""
 def lookup(obj):
+    """
+        This function list the attributes and
+        methods of object passed to it.
+    """
     if obj is None:
         return
     return dir(obj)
