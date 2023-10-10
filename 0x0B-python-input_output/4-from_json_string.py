1b81a9ca93e397367586610ede78e825b6370108
danny6200 <ugookorodaniel620@gmail.com>
2023-10-10 22:05:53 +0100

    Add: 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    This module supplies the from_json_string
+    function
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+        This function converts a json_string
+        to a python object
+    """
+    if my_str is None:
+        return
+
+    return json.loads(my_str)
