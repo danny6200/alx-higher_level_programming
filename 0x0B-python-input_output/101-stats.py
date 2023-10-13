cbb03b0ec0ac7c6c90f1c8a5590cd37c36219a2e
danny6200 <ugookorodaniel620@gmail.com>
2023-10-13 20:42:25 +0100

    Modify: 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,26 +21,28 @@ def main():
         File_size = 0
         status_codes = dict()
         line_buffer = []
+        possible_stat_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
         for line in sys.stdin:
 
+            line = line.strip()
             line_file_size = line.split()[-1]
             File_size += int(line_file_size)
             status_code = line.split()[-2]
 
-            if status_code in status_codes.keys():
-                status_codes[status_code] += 1
-            else:
-                status_codes[status_code] = 1
+            if status_code in possible_stat_code:
+                if status_code in status_codes.keys():
+                    status_codes[status_code] += 1
+                else:
+                    status_codes[status_code] = 1
 
             line_buffer.append(line)
 
             if len(line_buffer) == 10:
                 print("File size: {:d}".format(File_size))
                 for key, value in status_codes.items():
-                    print("{}: {:d}".format(key, value))
-                print()
-                
+                    print("{:d}: {:d}".format(int(key), value))
+
                 File_size = 0
                 status_codes = dict()
                 line_buffer = []
