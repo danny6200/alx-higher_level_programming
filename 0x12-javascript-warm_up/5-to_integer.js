a9364dc67765aefc1202b7fe30edbd7079500bf5
danny6200 <ugookorodaniel620@gmail.com>
2023-12-28 15:27:53 -0800

    Format: statement within the else block to meet  the requirement in 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const { argv } = require('process');
 if (argv[2] === undefined || isNaN(parseInt(argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(argv[2]));
+  console.log(`My number: ${parseInt(argv[2])}`);
 }
