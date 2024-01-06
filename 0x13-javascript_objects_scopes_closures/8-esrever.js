394836f02f57904cacab0e84910c87c485a81185
danny6200 <ugookorodaniel620@gmail.com>
2024-01-06 08:23:00 -0800

    Format: 8-esrever.js to be semistandard compliant

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reverse_list = [];
+  const reverseList = [];
   for (let i = 0, j = list.length - 1; i < list.length; i++, j--) {
-    reverse_list[i] = list[j];
+    reverseList[i] = list[j];
   }
-  return reverse_list;
+  return reverseList;
 };
